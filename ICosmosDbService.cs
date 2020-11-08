19a54ddb979ac5c543de54796466d263131e0789
Eswarasagar <67064493+Eswarasagar@users.noreply.github.com>
2020-11-08 13:52:37 -0600

    Add files via upload

---
+++ ICosmosDbService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace webapiTimewaste
+{
+
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+    }
+}
